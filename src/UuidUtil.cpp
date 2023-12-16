a7c87a5ea86c45319d40f293c3ded87436745ac7
John Wason <wason@wasontech.com>
2023-12-15 23:00:02 -0500

    Fix uuidutil exports

--- src/UuidUtil.cpp
+++ src/UuidUtil.cpp
@@ -7,7 +7,7 @@ namespace Companion
 {
 namespace Util
 {
-    static com::robotraconteur::uuid::UUID ParseUuid(const std::string& string_uuid)
+    com::robotraconteur::uuid::UUID ParseUuid(const std::string& string_uuid)
     {
         com::robotraconteur::uuid::UUID ret;
         boost::uuids::uuid boost_uuid = boost::lexical_cast<boost::uuids::uuid>(string_uuid);
@@ -15,28 +15,28 @@ namespace Util
         return ret;
     }
 
-    static std::string UuidToString(com::robotraconteur::uuid::UUID uuid)
+    std::string UuidToString(com::robotraconteur::uuid::UUID uuid)
     {
         boost::uuids::uuid boost_uuid;
         std::copy(uuid.a.begin(),uuid.a.end(),boost_uuid.begin());
         return boost::lexical_cast<std::string>(boost_uuid);
     }
 
-    static com::robotraconteur::uuid::UUID BoostUuidToUuid(const boost::uuids::uuid& boost_uuid)
+    com::robotraconteur::uuid::UUID BoostUuidToUuid(const boost::uuids::uuid& boost_uuid)
     {
         com::robotraconteur::uuid::UUID ret;
         std::copy(boost_uuid.begin(), boost_uuid.end(), ret.a.begin());
         return ret;
     }
 
-    static boost::uuids::uuid UuidToBoostUuid(const com::robotraconteur::uuid::UUID& uuid)
+    boost::uuids::uuid UuidToBoostUuid(const com::robotraconteur::uuid::UUID& uuid)
     {
         boost::uuids::uuid boost_uuid;
         std::copy(uuid.a.begin(), uuid.a.end(), boost_uuid.begin());
         return boost_uuid;
     }
 
-    static com::robotraconteur::uuid::UUID NewRandomUuid()
+    com::robotraconteur::uuid::UUID NewRandomUuid()
     {
         boost::uuids::uuid boost_uuid = boost::uuids::random_generator()();
         return BoostUuidToUuid(boost_uuid);
