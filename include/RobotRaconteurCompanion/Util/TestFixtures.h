8f80a203308d56628733b4a56574fa2b7833f256
John Wason <wason@wasontech.com>
2023-12-15 22:59:00 -0500

    Add documentation to TestFixtures.h

--- include/RobotRaconteurCompanion/Util/TestFixtures.h
+++ include/RobotRaconteurCompanion/Util/TestFixtures.h
@@ -1,3 +1,26 @@
+/**
+ * @file TestFixtures.h
+ *
+ * @author John Wason, PhD
+ *
+ * @copyright Copyright 2023 Wason Technology, LLC
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * @par
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <RobotRaconteur.h>
 
 #pragma once
@@ -23,10 +46,26 @@ namespace Util
     class IntraTestFixture
     {
     public:
+        /**
+         * @brief The client node
+         * 
+         */
         RR_SHARED_PTR<RobotRaconteur::RobotRaconteurNode> client_node;
+        /**
+         * @brief The server node
+         * 
+         */
         RR_SHARED_PTR<RobotRaconteur::RobotRaconteurNode> server_node;
 
+        /**
+         * @brief The client transport
+         * 
+         */
         RR_SHARED_PTR<RobotRaconteur::IntraTransport> client_transport;
+        /**
+         * @brief The server transport
+         * 
+         */
         RR_SHARED_PTR<RobotRaconteur::IntraTransport> server_transport;
 
         /**
