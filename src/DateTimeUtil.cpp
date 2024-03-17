db078aa48a7d504b852b59eb935ab4b447848dec
John Wason <wason@wasontech.com>
2024-03-17 01:53:50 -0400

    Move datetime util implementation into lib

---
+++ src/DateTimeUtil.cpp
@@ -0,0 +1,105 @@
+/**
+ * @file DateTimeUtil.cpp
+ *
+ * @author John Wason, PhD
+ *
+ * @copyright Copyright 2023 Wason Technology, LLC
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * @par
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "RobotRaconteurCompanion/Util/DateTimeUtil.h"
+
+namespace RobotRaconteur
+{
+namespace Companion
+{
+namespace Util
+{
+
+com::robotraconteur::datetime::DateTimeUTC UtcNow(RobotRaconteur::RobotRaconteurNodePtr node, com::robotraconteur::device::DeviceInfoPtr info)
+{
+    if (!node)
+    {
+        node = RobotRaconteur::RobotRaconteurNode::sp();        
+    }
+
+    RobotRaconteur::TimeSpec now = RobotRaconteur::ptimeToTimeSpec(node->NowUTC());
+    com::robotraconteur::datetime::DateTimeUTC ret;
+    memset(&ret, 0, sizeof(ret));
+    ret.seconds = now.seconds;
+    ret.nanoseconds = now.nanoseconds;
+    ret.clock_info.clock_type = (int)com::robotraconteur::datetime::ClockTypeCode::default_;
+    if (info)
+    {
+        if (info->device)
+        {
+            ret.clock_info.clock_uuid.a = info->device->uuid.a;
+        }
+    }
+
+    return ret;    
+}
+
+com::robotraconteur::datetime::TimeSpec2 TimeSpec2Now(RobotRaconteur::RobotRaconteurNodePtr node, com::robotraconteur::device::DeviceInfoPtr info)
+{
+    if (!node)
+    {
+        node = RobotRaconteur::RobotRaconteurNode::sp();        
+    }
+
+    RobotRaconteur::TimeSpec now = node->NowTimeSpec();
+    com::robotraconteur::datetime::TimeSpec2 ret;
+    memset(&ret, 0, sizeof(ret));
+    ret.seconds = now.seconds;
+    ret.nanoseconds = now.nanoseconds;
+    ret.clock_info.clock_type = (int)com::robotraconteur::datetime::ClockTypeCode::default_;
+    if (info)
+    {
+        if (info->device)
+        {
+            ret.clock_info.clock_uuid.a = info->device->uuid.a;
+        }
+    }
+
+    return ret;    
+}
+
+com::robotraconteur::datetime::TimeSpec3 TimeSpec3Now(RobotRaconteur::RobotRaconteurNodePtr node)
+{
+    if (!node)
+    {
+        node = RobotRaconteur::RobotRaconteurNode::sp();        
+    }
+
+    RobotRaconteur::TimeSpec now = node->NowTimeSpec();
+    com::robotraconteur::datetime::TimeSpec3 ret;
+    ret.s.microseconds = now.seconds*1000000 + now.nanoseconds/1000;    
+    return ret;
+}
+
+com::robotraconteur::device::clock::DeviceTime FillDeviceTime(RobotRaconteur::RobotRaconteurNodePtr node, com::robotraconteur::device::DeviceInfoPtr device_info, uint64_t seqno)
+{
+    com::robotraconteur::device::clock::DeviceTime ret;
+    ret.device_seqno = seqno;
+    ret.device_ts = TimeSpec2Now(node, device_info);
+    ret.device_utc = UtcNow(node, device_info);
+    return ret;
+}
+
+}
+}
+}
