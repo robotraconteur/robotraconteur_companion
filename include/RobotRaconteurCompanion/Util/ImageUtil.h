47a378930bfdbf25e82af23e20121bd622d3dd16
John Wason <wason@wasontech.com>
2024-05-26 04:38:30 -0400

    Improve ImageUtil and add tests

--- include/RobotRaconteurCompanion/Util/ImageUtil.h
+++ include/RobotRaconteurCompanion/Util/ImageUtil.h
@@ -7,7 +7,7 @@
 
 #include <boost/predef/other/endian.h>
 
-#if !defined(BOOST_ENDIAN_LITTLE_WORD) 
+#if !defined(BOOST_ENDIAN_LITTLE_BYTE_AVAILABLE)
 BOOST_STATIC_ASSERT(false);
 #endif
 
@@ -37,6 +37,7 @@ namespace Util
         int32_t width = image->image_info->width;
         int32_t height = image->image_info->height;
         int32_t step = image->image_info->step;
+        size_t expected_bytes = 0;
 
         if (width <= 0 || height <= 0)
         {
@@ -53,6 +54,7 @@ namespace Util
                 step = width * 3;
             }
             mat_type = CV_8UC3;
+            expected_bytes = width * height * 3;
             break;
         }
         case com::robotraconteur::image::ImageEncoding::rgba8888:
@@ -62,6 +64,7 @@ namespace Util
                 step = width * 4;
             }
             mat_type = CV_8UC4;
+            expected_bytes = width * height * 4;
             break;
         }
         case com::robotraconteur::image::ImageEncoding::bgr888:
@@ -71,6 +74,7 @@ namespace Util
                 step = width * 3;
             }
             mat_type = CV_8UC3;
+            expected_bytes = width * height * 3;
             break;
         }
         case com::robotraconteur::image::ImageEncoding::bgra8888:
@@ -80,6 +84,7 @@ namespace Util
                 step = width * 4;
             }
             mat_type = CV_8UC4;
+            expected_bytes = width * height * 4;
             break;
         }
         case com::robotraconteur::image::ImageEncoding::mono8:
@@ -89,6 +94,7 @@ namespace Util
                 step = width;
             }
             mat_type = CV_8UC1;
+            expected_bytes = width * height;
             break;
         }
         case com::robotraconteur::image::ImageEncoding::mono16:
@@ -99,6 +105,7 @@ namespace Util
                 step = width * 2;
             }
             mat_type = CV_16UC1;
+            expected_bytes = width * height * 2;
             break;
         }
         case com::robotraconteur::image::ImageEncoding::mono32:
@@ -109,6 +116,7 @@ namespace Util
                 step = width * 4;
             }
             mat_type = CV_32SC1;
+            expected_bytes = width * height * 4;
             break;
         }
         case com::robotraconteur::image::ImageEncoding::mono_f16:
@@ -118,15 +126,29 @@ namespace Util
                 step = width * 2;
             }
             mat_type = CV_16FC1;
+            expected_bytes = width * height * 2;
             break;
         }
         case com::robotraconteur::image::ImageEncoding::depth_f32:
+        case com::robotraconteur::image::ImageEncoding::mono_f32:
         {
             if (step <= 0)
             {
                 step = width * 4;
             }
             mat_type = CV_32FC1;
+            expected_bytes = width * height * 4;
+            break;
+        }
+        case com::robotraconteur::image::ImageEncoding::mono_f64:
+        case com::robotraconteur::image::ImageEncoding::depth_f64:
+        {
+            if (step <= 0)
+            {
+                step = width * 8;
+            }
+            mat_type = CV_64FC1;
+            expected_bytes = width * height * 8;
             break;
         }
         default:
@@ -162,12 +184,7 @@ namespace Util
             throw InvalidArgumentException("Invalid number of channels");
         }
 
-        if (mat.depth() <= 0)
-        {
-            throw InvalidArgumentException("Invalid depth");
-        }
-
-        if (mat.step <= 0)
+        if (mat.step[0] <= 0)
         {
             throw InvalidArgumentException("Invalid step");
         }
@@ -178,112 +195,103 @@ namespace Util
 
         image_info->width = mat.cols;
         image_info->height = mat.rows;
-        image_info->step = boost::numeric_cast<uint32_t>((size_t)mat.step);
+        image_info->step = boost::numeric_cast<uint32_t>((size_t)mat.step[0]);
         image_info->encoding = encoding;
+
+        int32_t mat_type = -1;
+        size_t element_size = 0;
 
         switch (encoding)
         {
         case com::robotraconteur::image::ImageEncoding::rgb888:
         {
-            if (mat.type() != CV_8UC3)
-            {
-                throw InvalidArgumentException("Invalid image type passed for encoding rgb888");
-            }
-            RobotRaconteur::RRArrayPtr<uint8_t> rr_data = RobotRaconteur::AllocateRRArray<uint8_t>(mat.total());
-            cv::Mat rr_mat_target(mat.rows, mat.cols, CV_8UC3, rr_data->data(), mat.step);
-            cv::cvtColor(mat, rr_mat_target, cv::COLOR_BGR2RGB);
-            image->data = rr_data;
-            return image;
+            mat_type = CV_8UC3;
+            element_size = 3;
+            break;
         }
         case com::robotraconteur::image::ImageEncoding::rgba8888:
         {
-            if (mat.type() != CV_8UC4)
-            {
-                throw InvalidArgumentException("Invalid image type passed for encoding rgba8888");
-            }
-            RobotRaconteur::RRArrayPtr<uint8_t> rr_data = RobotRaconteur::AllocateRRArray<uint8_t>(mat.total());
-            cv::Mat rr_mat_target(mat.rows, mat.cols, CV_8UC4, rr_data->data(), mat.step);
-            cv::cvtColor(mat, rr_mat_target, cv::COLOR_BGRA2RGBA);
-            image->data = rr_data;
-            return image;
+            mat_type = CV_8UC4;
+            element_size = 4;
+            break;
         }
         case com::robotraconteur::image::ImageEncoding::bgr888:
         {
-            if (mat.type() != CV_8UC3)
-            {
-                throw InvalidArgumentException("Invalid image type passed for encoding bgr888");
-            }
-            RobotRaconteur::RRArrayPtr<uint8_t> rr_data = RobotRaconteur::AllocateRRArray<uint8_t>(mat.total());
-            cv::Mat rr_mat_target(mat.rows, mat.cols, CV_8UC3, rr_data->data(), mat.step);
-            mat.copyTo(rr_mat_target);
-            image->data = rr_data;
-            return image;
+            mat_type = CV_8UC3;
+            element_size = 3;
+            break;
         }
         case com::robotraconteur::image::ImageEncoding::bgra8888:
         {
-            if (mat.type() != CV_8UC4)
-            {
-                throw InvalidArgumentException("Invalid image type passed for encoding bgra8888");
-            }
-            RobotRaconteur::RRArrayPtr<uint8_t> rr_data = RobotRaconteur::AllocateRRArray<uint8_t>(mat.total());
-            cv::Mat rr_mat_target(mat.rows, mat.cols, CV_8UC4, rr_data->data(), mat.step);
-            mat.copyTo(rr_mat_target);
-            image->data = rr_data;
-            return image;
+            mat_type = CV_8UC4;
+            element_size = 4;
+            break;
         }
         case com::robotraconteur::image::ImageEncoding::mono8:
         {
-            if (mat.type() != CV_8UC1)
-            {
-                throw InvalidArgumentException("Invalid image type passed for encoding mono8");
-            }
-            RobotRaconteur::RRArrayPtr<uint8_t> rr_data = RobotRaconteur::AllocateRRArray<uint8_t>(mat.total());
-            cv::Mat rr_mat_target(mat.rows, mat.cols, CV_8UC1, rr_data->data(), mat.step);
-            mat.copyTo(rr_mat_target);
-            image->data = rr_data;
-            return image;
+            mat_type = CV_8UC1;
+            element_size = 1;
+            break;
         }
         case com::robotraconteur::image::ImageEncoding::mono16:
         case com::robotraconteur::image::ImageEncoding::depth_u16:
         {
-            if (mat.type() != CV_16UC1)
-            {
-                throw InvalidArgumentException("Invalid image type passed for encoding mono16");
-            }
-            RobotRaconteur::RRArrayPtr<uint8_t> rr_data = RobotRaconteur::AllocateRRArray<uint8_t>(mat.total() * 2);
-            cv::Mat rr_mat_target(mat.rows, mat.cols, CV_16UC1, rr_data->data(), mat.step);
-            mat.copyTo(rr_mat_target);
-            image->data = rr_data;
-            return image;
+            mat_type = CV_16UC1;
+            element_size = 2;
+            break;
         }
         case com::robotraconteur::image::ImageEncoding::mono32:
         case com::robotraconteur::image::ImageEncoding::depth_u32:
         {
-            if (mat.type() != CV_32SC1)
-            {
-                throw InvalidArgumentException("Invalid image type passed for encoding mono32");
-            }
-            RobotRaconteur::RRArrayPtr<uint8_t> rr_data = RobotRaconteur::AllocateRRArray<uint8_t>(mat.total() * 4);
-            cv::Mat rr_mat_target(mat.rows, mat.cols, CV_32SC1, rr_data->data(), mat.step);
-            mat.copyTo(rr_mat_target);
-            image->data = rr_data;
-            return image;
+            mat_type = CV_32SC1;
+            element_size = 4;
+            break;
         }
+        case com::robotraconteur::image::ImageEncoding::mono_f16:
+        {
+            mat_type = CV_16FC1;
+            element_size = 2;
+            break;
+        }
+        case com::robotraconteur::image::ImageEncoding::mono_f32:
         case com::robotraconteur::image::ImageEncoding::depth_f32:
         {
-            if (mat.type() != CV_32FC1)
-            {
-                throw InvalidArgumentException("Invalid image type passed for encoding depth_f32");
-            }
-            RobotRaconteur::RRArrayPtr<uint8_t> rr_data = RobotRaconteur::AllocateRRArray<uint8_t>(mat.total() * 4);
-            cv::Mat rr_mat_target(mat.rows, mat.cols, CV_32FC1, rr_data->data(), mat.step);
-            mat.copyTo(rr_mat_target);
-            image->data = rr_data;
-            return image;
+            mat_type = CV_32FC1;
+            element_size = 4;
+            break;
+        }
+        case com::robotraconteur::image::ImageEncoding::mono_f64:
+        case com::robotraconteur::image::ImageEncoding::depth_f64:
+        {
+            mat_type = CV_64FC1;
+            element_size = 4;
+            break;
         }
         default:
             throw InvalidArgumentException("Unsupported image encoding");
         }
+
+        if (mat.type() != mat_type)
+        {
+            throw InvalidArgumentException("Invalid image type passed for specified encoding");
+        }
+        RobotRaconteur::RRArrayPtr<uint8_t> rr_data = RobotRaconteur::AllocateRRArray<uint8_t>(mat.total()*mat.elemSize());
+        cv::Mat rr_mat_target(mat.rows, mat.cols, mat_type, rr_data->data(), mat.step[0]);
+        switch (encoding)
+        {
+        case com::robotraconteur::image::ImageEncoding::rgb888:
+            cv::cvtColor(mat, rr_mat_target, cv::COLOR_BGR2RGB);
+            break;
+        case com::robotraconteur::image::ImageEncoding::rgba8888:
+            cv::cvtColor(mat, rr_mat_target, cv::COLOR_BGRA2RGBA);
+            break;
+        default:
+            mat.copyTo(rr_mat_target);
+            break;
+        }
+        image->data = rr_data;
+
+        return image;        
     }
 }
 }
--- test/test_image_util.cpp
+++ test/test_image_util.cpp
@@ -2,6 +2,163 @@
 
 #include <gtest/gtest.h>
 
+namespace RRC_Util = RobotRaconteur::Companion::Util;
+
+void compare_mat(const cv::Mat& img1, const cv::Mat& img2)
+{
+    ASSERT_EQ(img1.cols, img2.cols);
+    ASSERT_EQ(img1.rows, img2.rows);
+    ASSERT_EQ(img1.step[0], img2.step[0]);
+    ASSERT_EQ(img1.type(), img2.type());
+    ASSERT_EQ(memcmp(img1.data, img2.data, img1.total() * img1.elemSize()), 0);
+}
+
+void compare_image_info(const com::robotraconteur::image::ImageInfoPtr& info1, const cv::Mat& mat2, 
+    com::robotraconteur::image::ImageEncoding::ImageEncoding expected_encoding)
+{
+    ASSERT_EQ(info1->width, mat2.cols);
+    ASSERT_EQ(info1->height, mat2.rows);
+    ASSERT_EQ(info1->step, mat2.step[0]);
+    ASSERT_EQ(info1->encoding, expected_encoding);
+}
+
+void compare_image_bytes(const com::robotraconteur::image::ImagePtr& img1, const cv::Mat& img2)
+{
+    ASSERT_EQ(img1->data->size(), img2.total() * img2.elemSize());
+    ASSERT_EQ(memcmp(img1->data->data(), img2.data, img2.total() * img2.elemSize()), 0);
+}
+
+void compare_image_bytes_cvt_color(const com::robotraconteur::image::ImagePtr& img1, const cv::Mat& img2, int cvt_color)
+{
+    cv::Mat img2_cvt;
+    cv::cvtColor(img2, img2_cvt, cvt_color);
+    ASSERT_EQ(img1->data->size(), img2_cvt.total() * img2_cvt.elemSize());
+    ASSERT_EQ(memcmp(img1->data->data(), img2_cvt.data, img2_cvt.total() * img2_cvt.elemSize()), 0);
+}
+
+
+void run_image_test(const cv::Mat& img, com::robotraconteur::image::ImageEncoding::ImageEncoding encoding, int cvt_color = -1)
+{
+    // Convert to ImagePtr
+    auto img_ptr = RRC_Util::MatToImage(img, encoding);
+
+    // Check image_info
+    compare_image_info(img_ptr->image_info, img, encoding);
+
+    if (cvt_color != -1)
+    {
+        // Compare bytes
+        compare_image_bytes_cvt_color(img_ptr, img, cvt_color);
+    }
+    else
+    {
+        // Compare bytes
+        compare_image_bytes(img_ptr, img);
+    }
+    
+    // Convert back
+    auto img2 = RRC_Util::ImageToMat(img_ptr);
+
+    // Compare images
+    compare_mat(img, img2);
+}
+
+TEST(ImageUtil, TestImageUtil_BGR)
+{
+    // Populate random image
+    cv::Mat img(100, 110, CV_8UC3);
+    cv::randu(img, cv::Scalar(0, 0, 0), cv::Scalar(255, 255, 255));
+
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::bgr888);
+}
+
+TEST(ImageUtil, TestImageUtil_RGB)
+{
+    // Populate random image
+    cv::Mat img(100, 110, CV_8UC3);
+    cv::randu(img, cv::Scalar(0, 0, 0), cv::Scalar(255, 255, 255));
+
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::rgb888, cv::COLOR_BGR2RGB);
+}
+
+TEST(ImageUtil, TestImageUtil_RGBA)
+{
+    // Populate random image
+    cv::Mat img(100, 110, CV_8UC4);
+    cv::randu(img, cv::Scalar(0, 0, 0, 0), cv::Scalar(255, 255, 255, 255));
+
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::rgba8888, cv::COLOR_BGRA2RGBA);
+}
+
+TEST(ImageUtil, TestImageUtil_BGRA)
+{
+    // Populate random image
+    cv::Mat img(100, 110, CV_8UC4);
+    cv::randu(img, cv::Scalar(0, 0, 0, 0), cv::Scalar(255, 255, 255, 255));
+
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::bgra8888);
+}
+
+TEST(ImageUtil, TestImageUtil_Mono8)
+{
+    // Populate random image
+    cv::Mat img(100, 110, CV_8UC1);
+    cv::randu(img, cv::Scalar(0), cv::Scalar(255));
+
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::mono8);
+}
+
+TEST(ImageUtil, TestImageUtil_Mono16)
+{
+    // Populate random image
+    cv::Mat img(100, 110, CV_16UC1);
+    cv::randu(img, cv::Scalar(0), cv::Scalar(65535));
+
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::mono16);
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::depth_u16);
+}
+
+TEST(ImageUtil, TestImageUtil_Mono32)
+{
+    // Populate random image
+    cv::Mat img(100, 110, CV_32SC1);
+    cv::randu(img, cv::Scalar(INT_MIN), cv::Scalar(INT_MAX));
+
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::mono32);
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::depth_u32);
+}
+
+TEST(ImageUtil, TestImageUtil_MonoF16)
+{
+    // Populate random image
+    cv::Mat img(100, 110, CV_16FC1);
+    cv::randu(img, cv::Scalar(-1.0), cv::Scalar(1.0));
+
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::mono_f16);
+}
+
+TEST(ImageUtil, TestImageUtil_MonoF32)
+{
+    // Populate random image
+    cv::Mat img(100, 110, CV_32FC1);
+    cv::randu(img, cv::Scalar(-1.0), cv::Scalar(1.0));
+
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::mono_f32);
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::depth_f32);
+}
+
+TEST(ImageUtil, TestImageUtil_MonoF64)
+{
+    // Populate random image
+    cv::Mat img(100, 110, CV_64FC1);
+    cv::randu(img, cv::Scalar(-1.0), cv::Scalar(1.0));
+
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::mono_f64);
+    run_image_test(img, com::robotraconteur::image::ImageEncoding::depth_f64);
+}
+    
+
+
 int main(int argc, char** argv) // NOLINT
 {
     testing::InitGoogleTest(&argc, argv);
