20ec0f95a845a909e2045c042e43b30519aa8ed5
John Wason <wason@wasontech.com>
2024-05-26 21:03:54 -0400

    Initial work on DeviceConnector util

---
+++ include/RobotRaconteurCompanion/Util/DeviceConnector.h
@@ -0,0 +1,89 @@
+#include <RobotRaconteur/Subscription.h>
+#include <RobotRaconteur/RobotRaconteurNode.h>
+#include "com__robotraconteur__identifier.h"
+#include <yaml-cpp.hpp>
+
+#include "UtilMacros.h"
+
+#pragma once
+
+namespace RobotRaconteur
+{
+namespace Companion
+{
+namespace Util
+{
+struct ROBOTRACONTEUR_COMPANION_UTIL_API DeviceConnectorDetails
+{
+    std::string DeviceNickname;
+    com::robotraconteur::identifier::IdentifierPtr Device;
+    std::string SerialNumber;
+    std::vector<com::robotraconteur::identifier::IdentifierPtr> Tags;
+    std::vector<RobotRaconteur::ServiceSubscriptionFilterNodePtr> ServiceNodes;
+    std::vector<std::string> Urls;
+    std::vector<std::string> RootObjectType;
+    RobotRaconteur::ServiceSubscriptionFilterPtr SubscriptionFilter;
+    std::vector<std::string> TransportSchemes;
+    RobotRaconteur::ServiceSubscriptionFilterNodePtr UrlAuth;
+    int32_t MaxConnections = 0;
+    RobotRaconteur::ServiceSubscriptionFilterAttributeGroupOperation TagMatchOperation;
+
+    DeviceConnectorDetails(const std::string& device_nickname);
+    DeviceConnectorDetails(const std::string& device_nickname,
+                           const com::robotraconteur::identifier::IdentifierPtr& device,
+                           const std::vector<com::robotraconteur::identifier::IdentifierPtr>& tags = {},
+                           const std::vector<RobotRaconteur::ServiceSubscriptionFilterNodePtr>& service_nodes = {});
+    DeviceConnectorDetails(const std::string& device_nickname, const std::string& url,
+                           const RobotRaconteur::ServiceSubscriptionFilterNodePtr& url_auth = nullptr);
+    DeviceConnectorDetails(const std::string& device_nickname, const std::vector<std::string>& root_object_type,
+                           const RobotRaconteur::ServiceSubscriptionFilterPtr& subscription_filter);
+};
+
+class ROBOTRACONTEUR_COMPANION_UTIL_API DeviceConnector
+{
+  public:
+    DeviceConnector(RobotRaconteur::RobotRaconteurNodePtr node = nullptr, bool autoconnect = true);
+
+    void Init();
+    void Init(const std::vector<DeviceConnectorDetails>& devices);
+    void InitFromYamlStr(const std::string& yaml);
+    void InitFromYamlFile(const std::istream& file);
+    void InitFromYamlFile(const boost::filesystem::path& filename);
+
+    void AddDevice(const DeviceConnectorDetails& device_details, bool force_connect = false);
+
+    void RemoveDevice(const std::string& device_nickname, bool close = true);
+
+    void ConnectDevice(const std::string& device_nickname);
+
+    void DisconnectDevice(const std::string& device_nickname, bool close = true);
+
+    RobotRaconteur::ServiceSubscriptionPtr GetDevice(const std::string& device_nickname, bool force_create = false);
+
+    bool TryGetDevice(const std::string& device_nickname, RobotRaconteur::ServiceSubscriptionPtr& device);
+
+    void Close();
+
+    std::vector<std::string> GetDeviceNicknames();
+
+    RR_SHARED_PTR<RobotRaconteur::ServiceSubscriptionManager> GetServiceSubscriptionManager();
+
+  protected:
+    void DoUpdateDevice(const DeviceConnectorDetails& device_details, bool force_connect = false);
+
+    RobotRaconteur::RobotRaconteurNodePtr node;
+    bool autoconnect = true;
+    RR_SHARED_PTR<RobotRaconteur::ServiceSubscriptionManager> subscription_manager;
+};
+
+std::vector<DeviceConnectorDetails> LoadDeviceDetailsFromFile(const boost::filesystem::path& filename);
+
+std::vector<DeviceConnectorDetails> LoadDeviceDetailsFromString(const std::string& yaml_str);
+
+std::vector<DeviceConnectorDetails> LoadDeviceDetailsFromStream(std::istream& stream);
+
+std::vector<DeviceConnectorDetails> LoadDeviceDetailsFromYAMLNode(const YAML::Node& node);
+
+} // namespace Util
+} // namespace Companion
+} // namespace RobotRaconteur
---
+++ src/DeviceConnector.cpp
@@ -0,0 +1,370 @@
+#include <RobotRaconteurCompanion/Util/DeviceConnector.h>
+#include <RobotRaconteurCompanion/Util/IdentifierUtil.h>
+
+namespace RR = RobotRaconteur;
+
+namespace RobotRaconteur
+{
+namespace Companion
+{
+namespace Util
+{
+DeviceConnectorDetails::DeviceConnectorDetails(const std::string& device_nickname) : DeviceNickname(device_nickname) {}
+
+DeviceConnectorDetails::DeviceConnectorDetails(
+    const std::string& device_nickname, const com::robotraconteur::identifier::IdentifierPtr& device,
+    const std::vector<com::robotraconteur::identifier::IdentifierPtr>& tags,
+    const std::vector<RobotRaconteur::ServiceSubscriptionFilterNodePtr>& service_nodes)
+    : DeviceNickname(device_nickname), Device(device), Tags(tags), ServiceNodes(service_nodes)
+{}
+
+DeviceConnectorDetails::DeviceConnectorDetails(const std::string& device_nickname, const std::string& url,
+                                               const RobotRaconteur::ServiceSubscriptionFilterNodePtr& url_auth)
+    : DeviceNickname(device_nickname), Urls({url}), UrlAuth(url_auth)
+{}
+
+DeviceConnectorDetails::DeviceConnectorDetails(const std::string& device_nickname,
+                                               const std::vector<std::string>& root_object_type,
+                                               const RobotRaconteur::ServiceSubscriptionFilterPtr& subscription_filter)
+    : DeviceNickname(device_nickname), RootObjectType(root_object_type), SubscriptionFilter(subscription_filter)
+{}
+
+namespace detail
+{
+
+RR::ServiceSubscriptionFilterAttribute DeviceDetailsIdentifierToFilterAttribute(
+    const com::robotraconteur::identifier::IdentifierPtr& id)
+{
+    std::string uuid_str;
+    if (!IsIdentifierAnyUuid(id))
+    {
+        uuid_str = UuidToString(id->uuid);
+    }
+
+    return RR::CreateServiceSubscriptionFilterAttributeIdentifier(id->name, uuid_str);
+}
+
+boost::tuple<std::vector<std::string>, RR::ServiceSubscriptionFilterPtr> DeviceDetailsToSubscriptionFilter(
+    const RR::RobotRaconteurNodePtr& node, const DeviceConnectorDetails& device_details)
+{
+    if (device_details.DeviceNickname.empty())
+    {
+        throw RR::InvalidArgumentException("Device details to filter must contain device_nickname");
+    }
+
+    try
+    {
+        if (!device_details.Urls.empty())
+        {
+            throw RR::InvalidArgumentException("Device details to filter must not contain urls");
+        }
+
+        if (device_details.SubscriptionFilter)
+        {
+            throw RR::InvalidArgumentException("Device details to filter must not contain subscription filter");
+        }
+
+        RR::ServiceSubscriptionFilterPtr sub_filter = RR_MAKE_SHARED<RR::ServiceSubscriptionFilter>();
+
+        std::vector<std::string> root_object_type = {"com.robotraconteur.device.Device"};
+
+        if (!device_details.RootObjectType.empty())
+        {
+            root_object_type = device_details.RootObjectType;
+        }
+
+        sub_filter->MaxConnections = device_details.MaxConnections;
+
+        if (device_details.Device)
+        {
+            RR::ServiceSubscriptionFilterAttributeGroup dev_attr_group;
+            dev_attr_group.Attributes.push_back(DeviceDetailsIdentifierToFilterAttribute(device_details.Device));
+
+            sub_filter->Attributes.insert(std::make_pair("device", dev_attr_group));
+        }
+
+        if (!device_details.SerialNumber.empty())
+        {
+            RR::ServiceSubscriptionFilterAttributeGroup dev_attr_group;
+            dev_attr_group.Attributes.push_back(RR::ServiceSubscriptionFilterAttribute(device_details.SerialNumber));
+            sub_filter->Attributes.insert(std::make_pair("serial_number", dev_attr_group));
+        }
+
+        if (!device_details.Tags.empty())
+        {
+            RR::ServiceSubscriptionFilterAttributeGroup tag_grp;
+            for (auto& e : device_details.Tags)
+            {
+                tag_grp.Attributes.push_back(DeviceDetailsIdentifierToFilterAttribute(e));
+            }
+            tag_grp.Operation = device_details.TagMatchOperation;
+
+            sub_filter->Attributes.insert(std::make_pair("tags", tag_grp));
+        }
+
+        sub_filter->AttributesMatchOperation = RR::ServiceSubscriptionFilterAttributeGroupOperation_AND;
+
+        if (!device_details.TransportSchemes.empty())
+        {
+            sub_filter->TransportSchemes = device_details.TransportSchemes;
+        }
+
+        if (!device_details.ServiceNodes.empty())
+        {
+            sub_filter->Nodes = device_details.ServiceNodes;
+        }
+
+        return boost::make_tuple(root_object_type, sub_filter);
+    }
+    catch (const std::exception& e)
+    {
+        throw RR::InvalidArgumentException("Invalid device details for device " + device_details.DeviceNickname + ": " +
+                                           e.what());
+    }
+}
+
+RR::ServiceSubscriptionManagerDetails device_details_to_subscription_details(
+    const RR::RobotRaconteurNodePtr& node, const std::string& nickname, const DeviceConnectorDetails& device_details)
+{
+    if (!device_details.Urls.empty())
+    {
+        std::string username;
+        RR::RRMapPtr<std::string, RR::RRValue> credentials;
+        if (device_details.UrlAuth)
+        {
+            username = device_details.UrlAuth->Username;
+            credentials = device_details.UrlAuth->Credentials;
+        }
+
+        RR::ServiceSubscriptionManagerDetails d;
+        d.Name = nickname;
+        d.ConnectionMethod = RR::ServiceSubscriptionManager_CONNECTION_METHOD_URL;
+        d.Urls = device_details.Urls;
+        d.UrlUsername = username;
+        d.UrlCredentials = credentials;
+        return d;
+    }
+    else if (device_details.SubscriptionFilter)
+    {
+        RR::ServiceSubscriptionManagerDetails d;
+        d.Name = nickname;
+        d.ConnectionMethod = RR::ServiceSubscriptionManager_CONNECTION_METHOD_TYPE;
+        d.Filter = device_details.SubscriptionFilter;
+        d.ServiceTypes = device_details.RootObjectType;
+        return d;
+    }
+    else
+    {
+        boost::tuple<std::vector<std::string>, RR::ServiceSubscriptionFilterPtr> filter =
+            DeviceDetailsToSubscriptionFilter(node, device_details);
+        RR::ServiceSubscriptionManagerDetails d;
+        d.Name = nickname;
+        d.ConnectionMethod = RR::ServiceSubscriptionManager_CONNECTION_METHOD_TYPE;
+        d.Filter = filter.get<1>();
+        d.ServiceTypes = filter.get<0>();
+        return d;
+    }
+}
+} // namespace detail
+
+namespace YAML
+{
+template <>
+struct convert<DeviceConnectorDetails>
+{
+    static Node encode(const DeviceConnectorDetails& rhs)
+    {
+        RR_UNUSED(rhs);
+        Node node;
+        return node;
+    }
+
+    static bool decode(const Node& node, DeviceConnectorDetails& rhs)
+    {
+        if (node["device"])
+        {
+            rhs.Device = node["device"].as<com::robotraconteur::identifier::IdentifierPtr>();
+        }
+        if (node["serial_number"])
+        {
+            rhs.SerialNumber = node["serial_number"].as<std::string>();
+        }
+        if (node["tags"])
+        {
+            rhs.Tags = node["tags"].as<std::vector<com::robotraconteur::identifier::IdentifierPtr> >();
+        }
+        if (node["urls"])
+        {
+            // rhs.Urls = node["urls"].as<std::vector<std::string>>();
+
+            // Allow Urls to be string or list
+            if (node["urls"].IsSequence())
+            {
+                rhs.Urls = node["urls"].as<std::vector<std::string> >();
+            }
+            else
+            {
+                rhs.Urls.push_back(node["urls"].as<std::string>());
+            }
+        }
+
+        return true;
+    }
+};
+
+struct DeviceConnectorDetailsFile
+{
+    std::map<std::string, DeviceConnectorDetails> devices;
+
+    std::vector<DeviceConnectorDetails> GetDevices()
+    {
+        std::vector<DeviceConnectorDetails> o;
+        for (auto& e : devices)
+        {
+            o.push_back(e.second);
+        }
+        return o;
+    }
+};
+
+template <>
+struct convert<DeviceConnectorDetailsFile>
+{
+    static Node encode(const DeviceConnectorDetailsFile& rhs)
+    {
+        RR_UNUSED(rhs);
+        Node node;
+        return node;
+    }
+
+    static bool decode(const Node& node, DeviceConnectorDetailsFile& rhs)
+    {
+        if (node["devices"])
+        {
+            rhs.devices = node["devices"].as<std::map<std::string, DeviceConnectorDetails> >();
+        }
+        // Set device_nickname to key
+        for (auto& e : rhs.devices)
+        {
+            e.second.DeviceNickname = e.first;
+        }
+        return true;
+    }
+};
+
+} // namespace YAML
+
+std::vector<DeviceConnectorDetails> LoadDeviceDetailsFromFile(const boost::filesystem::path& filename)
+{
+    auto map = YAML::LoadFile(filename.string());
+    return map.as<DeviceConnectorDetailsFile>().GetDevices();
+}
+
+std::vector<DeviceConnectorDetails> LoadDeviceDetailsFromString(const std::string& yaml_str)
+{
+    auto map = YAML::Load(yaml_str);
+    return map.as<DeviceConnectorDetailsFile>().GetDevices();
+}
+
+std::vector<DeviceConnectorDetails> LoadDeviceDetailsFromStream(std::istream& stream)
+{
+    auto map = YAML::Load(stream);
+    return map.as<DeviceConnectorDetailsFile>().GetDevices();
+}
+
+std::vector<DeviceConnectorDetails> LoadDeviceDetailsFromYAMLNode(const YAML::Node& node)
+{
+    return node.as<DeviceConnectorDetailsFile>().GetDevices();
+}
+
+DeviceConnector::DeviceConnector(RobotRaconteur::RobotRaconteurNodePtr node, bool autoconnect)
+    : autoconnect(autoconnect)
+{
+    if (!node)
+    {
+        node = RobotRaconteur::RobotRaconteurNode::sp();
+    }
+    this->node = node;
+}
+
+void DeviceConnector::Init()
+{
+    subscription_manager = RR_MAKE_SHARED<RobotRaconteur::ServiceSubscriptionManager>(node);
+}
+void DeviceConnector::Init(const std::vector<DeviceConnectorDetails>& devices)
+{
+    Init();
+    for (auto& e : devices)
+    {
+        AddDevice(e);
+    }
+}
+
+void DeviceConnector::InitFromYamlStr(const std::string& yaml)
+{
+    auto devices = LoadDeviceDetailsFromString(yaml);
+    Init(devices);
+}
+
+void DeviceConnector::InitFromYamlFile(const std::istream& file)
+{
+    auto devices = LoadDeviceDetailsFromStream(file);
+    Init(devices);
+}
+
+void DeviceConnector::InitFromYamlFile(const boost::filesystem::path& filename)
+{
+    auto devices = LoadDeviceDetailsFromFile(filename);
+    Init(devices);
+}
+
+void DeviceConnector::AddDevice(const DeviceConnectorDetails& device_details, bool force_connect)
+{
+    DoUpdateDevice(device_details, force_connect);
+}
+
+void DeviceConnector::RemoveDevice(const std::string& device_nickname, bool close)
+{
+    subscription_manager->RemoveSubscription(device_nickname, close);
+}
+
+void DeviceConnector::ConnectDevice(const std::string& device_nickname)
+{
+    subscription_manager->EnableSubscription(device_nickname);
+}
+
+void DeviceConnector::DisconnectDevice(const std::string& device_nickname, bool close)
+{
+    subscription_manager->DisableSubscription(device_nickname, close);
+}
+
+RobotRaconteur::ServiceSubscriptionPtr DeviceConnector::GetDevice(const std::string& device_nickname, bool force_create)
+{
+    return subscription_manager->GetSubscription(device_nickname, force_create);
+}
+
+bool DeviceConnector::TryGetDevice(const std::string& device_nickname, RobotRaconteur::ServiceSubscriptionPtr& device)
+{
+    try
+    {
+        device = subscription_manager->GetSubscription(device_nickname, device);
+        return true;
+    }
+    catch (const std::exception&)
+    {
+        return false;
+    }
+}
+
+void DeviceConnector::Close() { subscription_manager->Close(); }
+
+std::vector<std::string> DeviceConnector::GetDeviceNicknames() { return subscription_manager->GetSubscriptionNames(); }
+
+RR_SHARED_PTR<RobotRaconteur::ServiceSubscriptionManager> DeviceConnector::GetServiceSubscriptionManager()
+{
+    return subscription_manager;
+}
+
+} // namespace Util
+} // namespace Companion
+} // namespace RobotRaconteur
